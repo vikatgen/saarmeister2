83c40c255a5fac7aeda2dfde6a14405251857f16
vikatgen <47703789+vikatgen@users.noreply.github.com>
2019-11-01 22:51:00 +0200

    Some fixes and betas

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Smooth Scrolling for links and buttons with Jquery
 
-$(document).on("click", 'a[href^="#"]', function(event) {
+/*$(document).on("click", 'a[href^="#"]', function(event) {
   event.preventDefault();
 
   $("html, body").animate(
@@ -23,4 +23,19 @@ window.onscroll = function() {
   } else {
     navbar.classList.remove("navactive");
   }
-};
+}; */
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  toggleClasses();
+});
+
+function toggleClasses() {
+  hamburger.classList.toggle("open");
+  menu.classList.toggle("open");
+}
+
+setTimeout(toggleClasses, 500);
+setTimeout(toggleClasses, 3000);
